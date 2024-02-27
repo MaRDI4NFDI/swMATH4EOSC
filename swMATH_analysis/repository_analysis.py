d7536a2e8c1c88b3f34baf36a180cfd7aed5b214
Mazztok45 <maxence.azzouz@gmail.com>
2024-02-27 17:54:48 +0100

    creating index_init.txt

--- swMATH_analysis/repository_analysis.py
+++ swMATH_analysis/repository_analysis.py
@@ -1,1 +1,13 @@
 # -*- coding: utf-8 -*-
+import pandas
+import requests
+
+def final_xml2(de, api_source):
+
+    headers = {'Accept': 'application/json'}
+    r = requests.get(api_source + de, headers=headers)
+    if r.status_code != 200:
+        raise Exception(f"Unexpected response with status code {r.status_code}: {r.text}")
+    json = r.json()
+    # Bugfix as the sates are lists of lists which has no canonical XML mapping
+    if type(json['result'])==dict:
