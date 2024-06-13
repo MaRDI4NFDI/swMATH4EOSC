b7cbb2406536f25bc447e598646910416e0cf306
Mazztok45 <maxence.azzouz@gmail.com>
2024-06-13 16:18:36 +0200

    changing parameter to read files

--- user_documentation/swMATH2CodeMeta_formatting.py
+++ user_documentation/swMATH2CodeMeta_formatting.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df1 = pd.read_csv('swmath_to_codemeta.csv',skiprows=2,header=None,names=['swMATH','Property'])
+df1 = pd.read_csv('swmath_to_codemeta.csv',skiprows=1,header=None,names=['swMATH','Property','Schema.org'])
 df2 = pd.read_csv('Cargo.csv')
 
 df = pd.merge(df2,df1, on='Property',how='outer',)
